77c6ea7dc7717d36876a89941ad47d40b1da41d3
JonathanERC <jonathanestebanrondon@gmail.com>
2021-04-01 11:55:57 -0400

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import subprocess
 rutaDatos = os.path.expanduser('~/IA')
 carpetaDatos = os.path.expanduser(rutaDatos + '/'+'Data')
 
+#Definición del metodo que corre el entrenamiento del modelo
 def entrenarModelo():
     print('Modelo no entrenado')
     print('Inicializando entrenamiento...')
@@ -14,6 +15,7 @@ def entrenarModelo():
     print('Reconocimiento Facial con Emociones...')
     subprocess.call(['python','reconocimientoEmociones.py'], shell=True)
 
+#Condición que detecta si las dependencias del modelo existen, en caso contrario las instala
 if not os.path.exists(rutaDatos and carpetaDatos):
     print('Dependencias no encontradas...')
     print('Instalando dependencias...')
@@ -22,7 +24,9 @@ if not os.path.exists(rutaDatos and carpetaDatos):
     subprocess.call(['pip','install', 'numpy'], shell=True)
     subprocess.call(['pip','install', 'times'], shell=True)
     entrenarModelo()
+#Condición que detecta si la carpeta de datos existe para entrenar el modelo
 elif not os.path.exists(carpetaDatos):
     entrenarModelo()
+#Si ninguna de las condiciones anteriores se dan, entonces correr el programa
 else:
     subprocess.call(['python', 'reconocimientoEmociones.py'], shell=True)
